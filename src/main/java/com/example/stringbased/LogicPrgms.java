2293be04e1a9ffd89c501769a50aab291578166f
Kathir-N <45091461+Kathir-N@users.noreply.github.com>
2026-04-28 17:21:24 +0530

    Update LogicPrgms.java

--- src/main/java/com/example/stringbased/LogicPrgms.java
+++ src/main/java/com/example/stringbased/LogicPrgms.java
@@ -55,5 +55,43 @@ List<Item> result = allOrders.stream()
     .limit(3)                                                    // ✅ take top 3
     .collect(Collectors.toList());                               // ✅ collect
  
-   
+   ------------------------------------------
+
+Input: [2, 1, 5, 1, 3, 2], k = 3
+
+
+1. Take first k numbers and add them.
+  [2, 1, 5], 1, 3, 2
+sum = 2 + 1 + 5 = 8
+maxSum = 8
+2. Slide one step → add new, subtract old.
+  2, [1, 5, 1], 3, 2
+       ↑ new (1)
+   ↑ old leaving (2)
+
+sum = 8 + 1 - 2 = 7
+maxSum = 8  (8 is still bigger)
+
+  2, 1, [5, 1, 3], 2
+          ↑ new (3)
+      ↑ old leaving (1)
+
+sum = 7 + 3 - 1 = 9
+maxSum = 9  ✅ (new biggest!)
+
+  2, 1, 5, [1, 3, 2]
+              ↑ new (2)
+          ↑ old leaving (5)
+
+sum = 9 + 2 - 5 = 6
+maxSum = 9  (9 is still bigger)
+  
+  3. Keep track of the biggest sum you see. with example
+
+All sums seen: 8, 7, 9, 6
+Biggest sum   = 9  ✅
+  
+
+
+  
 }
